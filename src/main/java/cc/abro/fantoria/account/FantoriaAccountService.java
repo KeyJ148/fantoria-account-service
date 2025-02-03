43bd7517ab4d69ef51c5a6d68570a4cc880d8049
Evgeniy Serov <kejik148@gmail.com>
2025-02-03 22:01:48 +0500

    Add main class and context test

---
+++ src/main/java/cc/abro/fantoria/account/FantoriaAccountService.java
@@ -0,0 +1,13 @@
+package cc.abro.fantoria.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantoriaAccountService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantoriaAccountService.class, args);
+    }
+
+}
---
+++ src/test/java/cc/abro/fantoria/account/FantoriaAccountServiceTests.java
@@ -0,0 +1,15 @@
+package cc.abro.fantoria.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"local", "test"})
+class FantoriaAccountServiceTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
